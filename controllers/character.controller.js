575098e619dd7f903a8227b55cf1d6108a0ea716
Aurélien <aurelien.strimelle@bstorm.be>
2023-01-05 15:13:29 +0100

    Get All Character 🤼

--- controllers/character.controller.js
+++ controllers/character.controller.js
@@ -5,8 +5,16 @@ const characters = require('../data/characters.json');
 const CharacterController = {
 
     getAll: (req, res) => {
+        // Préparation des données à envoyer
+        const data = characters.map(c => ({
+            id: c.id,
+            firstname: c.firstname,
+            lastname: c.lastname
+        }));
 
-        res.sendStatus(501);
+        // Envoi des données sous forme de JSON
+        res.json(data);
+        // res.send(JSON.stringify(data));
     },
 
     getById: (req, res) => {
